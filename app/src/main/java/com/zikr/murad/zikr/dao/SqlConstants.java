fee8eff69c72ab4e52e8b488872ca0ef87c47624
xakus <xakus22@gmail.com>
2018-02-03 14:05:37 +0400

    zikrActivity

--- app/src/main/java/com/zikr/murad/zikr/dao/SqlConstants.java
+++ app/src/main/java/com/zikr/murad/zikr/dao/SqlConstants.java
@@ -8,100 +8,148 @@ public class SqlConstants {
 //            ------------- SQLite3 Dump File -------------
 //
 
-    public static final int    DATA_BASE_VERSION = 1;
+    public static final int DATA_BASE_VERSION = 1;
     public static final String DATA_BASE_NAME = "zikr";
-//            --  Tabel Names
-    public static final String TN_DICTIONARY="DICTIONARY";
-    public static final String TN_DICTIONARY_TYPE="DICTIONARY_TYPE";
-    public static final String TN_ORIGINAL_TRANSLATE="ORIGINAL_TRANSLATE";
-    public static final String TN_SOUND="SOUND";
-    public static final String TN_WHEN_THEY_READ_ZIKR="WHEN_THEY_READ_ZIKR";
-    public static final String TN_ZIKR_ORIGINAL="ZIKR_ORIGINAL";
-    public static final String TN_ZIKR_TRANSLATE="ZIKR_TRANSLATE";
-//            -- ------------------------------------------
+    //            --  Tabel Names
+    public static final String TN_DICTIONARY = "DICTIONARY";
+    public static final String TN_DICTIONARY_TYPE = "DICTIONARY_TYPE";
+    public static final String TN_ORIGINAL_TRANSLATE = "ORIGINAL_TRANSLATE";
+    public static final String TN_SOUND = "SOUND";
+    public static final String TN_WHEN_THEY_READ_ZIKR = "WHEN_THEY_READ_ZIKR";
+    public static final String TN_ZIKR_ORIGINAL = "ZIKR_ORIGINAL";
+    public static final String TN_ZIKR_TRANSLATE = "ZIKR_TRANSLATE";
+    //            -- ------------------------------------------
 //            -- Dump of "DICTIONARY"
 //            -- ------------------------------------------
     public static final String CREATE_DICTIONARY = "CREATE TABLE \"DICTIONARY\"(\n" +
-            "            "+DICTIONARY.CN_ID+" Integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
-            " "+DICTIONARY.CN_DT_ID+" Integer,\n" +
-            "            "+DICTIONARY.CN_NAME+" Text,\n" +
-            "            "+DICTIONARY.CN_CREATE_DATE+" Text,\n" +
-            "            "+DICTIONARY.CN_ACTIVE+" Integer )";
+            "            " + DICTIONARY.CN_ID + " Integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+            "            " + DICTIONARY.CN_DT_ID + " Integer,\n" +
+            "            " + DICTIONARY.CN_NAME + " Text,\n" +
+            "            " + DICTIONARY.CN_CREATE_DATE + " Text,\n" +
+            "            " + DICTIONARY.CN_ACTIVE + " Integer )";
 
-//
+    public class DICTIONARY {
+        public static final String CN_ID = "ID";
+        public static final String CN_DT_ID = "DT_ID";
+        public static final String CN_NAME = "NAME";
+        public static final String CN_CREATE_DATE = "CREATE_DATE";
+        public static final String CN_ACTIVE = "ACTIVE";
+    }
+
+    //
 //            -- ------------------------------------------
 //            -- Dump of "DICTIONARY_TYPE"
 //            -- ------------------------------------------
-    public static final String CREATE_DICTIONARY_TYPE = "CREATE TABLE "+TN_DICTIONARY_TYPE+"(\n" +
-            "            \"ID\" Integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
-            "\t\"NAME\" Text,\n" +
-            "            \"CREATE_DATE\" Text,\n" +
-            "            \"ACTIVE\" Integer )";
+    public static final String CREATE_DICTIONARY_TYPE = "CREATE TABLE " + TN_DICTIONARY_TYPE + "(\n" +
+            "            " + DICTIONARY_TYPE.CN_ID + " Integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+            "            " + DICTIONARY_TYPE.CN_NAME + " Text,\n" +
+            "            " + DICTIONARY_TYPE.CN_CREATE_DATE + " Text,\n" +
+            "            " + DICTIONARY_TYPE.CN_ACTIVE + " Integer )";
+
+    public class DICTIONARY_TYPE {
+        public static final String CN_ID = "ID";
+        public static final String CN_NAME = "NAME";
+        public static final String CN_CREATE_DATE = "CREATE_DATE";
+        public static final String CN_ACTIVE = "ACTIVE";
+    }
 
 
-//
+    //
 //            -- ------------------------------------------
 //            -- Dump of "ORIGINAL_TRANSLATE"
 //            -- ------------------------------------------
-    public static final String CREATE_ORIGINAL_TRANSLATE = "  CREATE TABLE "+TN_ORIGINAL_TRANSLATE+"(\n" +
-            "            \"ID\" Integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
-            "\t\"OR_ID\" Integer,\n" +
-            "            \"TR_ID\" Integer,\n" +
-            "            \"CREATE_DATE\" Text,\n" +
-            "            \"ACTIVE\" Integer )";
+    public static final String CREATE_ORIGINAL_TRANSLATE = "  CREATE TABLE " + TN_ORIGINAL_TRANSLATE + "(\n" +
+            "            " + ORIGINAL_TRANSLATE.CN_ID + " Integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+            "            " + ORIGINAL_TRANSLATE.CN_OR_ID + " Integer,\n" +
+            "            " + ORIGINAL_TRANSLATE.CN_TR_ID + " Integer,\n" +
+            "            " + ORIGINAL_TRANSLATE.CN_CREATE_DATE + " Text,\n" +
+            "            " + ORIGINAL_TRANSLATE.CN_ACTIVE + " Integer )";
 
-//
+    public class ORIGINAL_TRANSLATE {
+        public static final String CN_ID = "ID";
+        public static final String CN_OR_ID = "OR_ID";
+        public static final String CN_TR_ID = "TR_ID";
+        public static final String CN_CREATE_DATE = "CREATE_DATE";
+        public static final String CN_ACTIVE = "ACTIVE";
+    }
+
+    //
 //            -- ------------------------------------------
 //            -- Dump of "SOUND"
 //            -- ------------------------------------------
-    public static final String CREATE_SOUND = " CREATE TABLE "+TN_SOUND+"(\n" +
-            "            \"ID\" Integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
-            "\t\"PATH\" Text,\n" +
-            "            \"DIC_ID\" Integer,\n" +
-            "            \"ZIKR_ID\" Integer,\n" +
-            "            \"CREATE_DATE\" Text,\n" +
-            "            \"ACTIVE\" Integer )";
+    public static final String CREATE_SOUND = " CREATE TABLE " + TN_SOUND + "(\n" +
+            "            " + SOUND.CN_ID + " Integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+            "            " + SOUND.CN_PATH + " Text,\n" +
+            "            " + SOUND.CN_DIC_ID + " Integer,\n" +
+            "            " + SOUND.CN_ZIKR_ID + " Integer,\n" +
+            "            " + SOUND.CN_CREATE_DATE + " Text,\n" +
+            "            " + SOUND.CN_ACTIVE + " Integer )";
 
-//
+    public class SOUND {
+        public static final String CN_ID = "ID";
+        public static final String CN_PATH = "PATH";
+        public static final String CN_DIC_ID = "DIC_ID";
+        public static final String CN_ZIKR_ID = "ZIKR_ID";
+        public static final String CN_CREATE_DATE = "CREATE_DATE";
+        public static final String CN_ACTIVE = "ACTIVE";
+    }
+
+    //
 //            -- ------------------------------------------
 //            -- Dump of "WHEN_THEY_READ_ZIKR"
 //            -- ------------------------------------------
-    public static final String CREATE_WHEN_THEY_READ_ZIKR = " CREATE TABLE "+TN_WHEN_THEY_READ_ZIKR+"(\n" +
-            "            \"ID\" Integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
-            "\t\"D_ID\" Integer,\n" +
-            "            \"ZIKR_ID\" Integer,\n" +
-            "            \"CREATE_DATE\" Text,\n" +
-            "            \"ACTIVE\" Integer )";
+    public static final String CREATE_WHEN_THEY_READ_ZIKR = " CREATE TABLE " + TN_WHEN_THEY_READ_ZIKR + "(\n" +
+            "            " + WHEN_THEY_READ_ZIKR.CN_ID + " Integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+            "            " + WHEN_THEY_READ_ZIKR.CN_D_ID + " Integer,\n" +
+            "            " + WHEN_THEY_READ_ZIKR.CN_ZIKR_ID + " Integer,\n" +
+            "            " + WHEN_THEY_READ_ZIKR.CN_CREATE_DATE + " Text,\n" +
+            "            " + WHEN_THEY_READ_ZIKR.CN_ACTIVE + " Integer )";
 
-//
+    public class WHEN_THEY_READ_ZIKR {
+        public static final String CN_ID = "ID";
+        public static final String CN_D_ID = "D_ID";
+        public static final String CN_ZIKR_ID = "ZIKR_ID";
+        public static final String CN_CREATE_DATE = "CREATE_DATE";
+        public static final String CN_ACTIVE = "ACTIVE";
+    }
+
+    //
 //            -- ------------------------------------------
 //            -- Dump of "ZIKR_ORIGINAL"
 //            -- ------------------------------------------
-    public static final String CREATE_ZIKR_ORIGINAL = "    CREATE TABLE "+TN_ZIKR_ORIGINAL+"(\n" +
-            "            \"ID\" Integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
-            "\t\"ZIKR_OR\" Text,\n" +
-            "            \"COUNT\" Integer,\n" +
-            "            \"CREATE_DATE\" Text,\n" +
-            "            \"ACTIVE\" Integer DEFAULT 1 )";
+    public static final String CREATE_ZIKR_ORIGINAL = "    CREATE TABLE " + TN_ZIKR_ORIGINAL + "(\n" +
+            "            " + ZIKR_ORIGINAL.CN_ID + " Integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+            "            " + ZIKR_ORIGINAL.CN_ZIKR_OR + " Text,\n" +
+            "            " + ZIKR_ORIGINAL.CN_COUNT + " Integer,\n" +
+            "            " + ZIKR_ORIGINAL.CN_CREATE_DATE + " Text,\n" +
+            "            " + ZIKR_ORIGINAL.CN_ACTIVE + " Integer DEFAULT 1 )";
+
+    public class ZIKR_ORIGINAL {
+        public static final String CN_ID = "ID";
+        public static final String CN_ZIKR_OR = "ZIKR_OR";
+        public static final String CN_COUNT = "COUNT";
+        public static final String CN_CREATE_DATE = "CREATE_DATE";
+        public static final String CN_ACTIVE = "ACTIVE";
+    }
+
     //
 //
 //            -- ------------------------------------------
 //            -- Dump of "ZIKR_TRANSLATE"
 //            -- ------------------------------------------
-    public static final String CREATE_ZIKR_TRANSLATE = " CREATE TABLE "+TN_ZIKR_TRANSLATE+"(\n" +
-            "            \"ID\" Integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
-            "\t\"ZIKR_TR\" Text,\n" +
-            "            \"DICTIONARY_ID\" Integer,\n" +
-            "            \"CREATE_DATE\" Text,\n" +
-            "            \"ACTIVE\" Integer )";
+    public static final String CREATE_ZIKR_TRANSLATE = " CREATE TABLE " + TN_ZIKR_TRANSLATE + "(\n" +
+            "            " + ZIKR_TRANSLATE.CN_ID + " Integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+            "            " + ZIKR_TRANSLATE.CN_ZIKR_TR + " Text,\n" +
+            "            " + ZIKR_TRANSLATE.CN_DICTIONARY_ID + " Integer,\n" +
+            "            " + ZIKR_TRANSLATE.CN_CREATE_DATE + " Text,\n" +
+            "            " + ZIKR_TRANSLATE.CN_ACTIVE + " Integer )";
 
-
-public class DICTIONARY{
-    public static final String CN_ID="ID";
-    public static final String CN_DT_ID="DT_ID";
-    public static final String CN_NAME="NAME";
-    public static final String CN_CREATE_DATE="CREATE_DATE";
-    public static final String CN_ACTIVE="ACTIVE";
-}
+    public class ZIKR_TRANSLATE {
+        public static final String CN_ID = "ID";
+        public static final String CN_ZIKR_TR = "ZIKR_TR";
+        public static final String CN_DICTIONARY_ID = "DICTIONARY_ID";
+        public static final String CN_CREATE_DATE = "CREATE_DATE";
+        public static final String CN_ACTIVE = "ACTIVE";
+    }
 
 }
--- app/src/main/java/com/zikr/murad/zikr/view/MainActivity.java
+++ app/src/main/java/com/zikr/murad/zikr/view/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zikr.murad.zikr.view;
 
+import android.content.Intent;
 import android.os.CountDownTimer;
 import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.zikr.murad.zikr.R;
+import com.zikr.murad.zikr.ZikrActivity;
 
 public class MainActivity extends AppCompatActivity {
     private TextView salam1;
@@ -23,25 +25,6 @@ public class MainActivity extends AppCompatActivity {
         salam2= (TextView) findViewById(R.id.tv_salam2);
         salam3= (TextView) findViewById(R.id.tv_salam3);
 
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                float t=0;
-//                while (t<=time+2){
-//                    t+=0.00005;
-//                    if((time/3)<=t){
-//                        salam1.setVisibility(View.VISIBLE);
-//                    }
-//                    if((time/2)<=t){
-//                        salam2.setVisibility(View.VISIBLE);
-//                    }
-//                    if((time)<=t){
-//                        salam3.setVisibility(View.VISIBLE);
-//                    }
-//                    Log.d("time", t+"");
-//                }
-//            }
-//        }).start();
         new CountDownTimer(8000, 1000) {
 
             public void onTick(long millisUntilFinished) {
@@ -59,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
             }
 
             public void onFinish() {
-                Log.d("time","done!");
+                Intent intent =new Intent(MainActivity.this, ZikrActivity.class);
+                startActivity(intent);
             }
         }.start();
 
